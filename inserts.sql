f9f7d595f9dadaa5113a6c187308f9d0813eb4f9
devhiggins <devinhiggins13@gmail.com>
2019-07-17 06:27:19 -0400

    more inserts

---
+++ inserts.sql
@@ -0,0 +1,24 @@
+INSERT INTO User 
+	VALUES ("Devin", "Devin", "D", 'Higgins', "pass", 'devin@gmail.com');
+    
+INSERT INTO User 
+	VALUES ("Logan", "Logan", "L", 'Short', "pass", 'logan@gmail.com');
+    
+INSERT INTO User 
+	VALUES ("Alexander", "Alexander", "A", 'Schott', "pass", 'alexander@gmail.com');
+
+INSERT INTO User
+	VALUES("admin", "first", "m" "last", "pass", "admin@gmail.com");
+    
+INSERT INTO Admin
+	VALUES("admin");
+    
+INSERT INTO Station
+	VALUES("espanya", "open", "cat", "placa espanya", 3, "barcelona" );
+    
+INSERT INTO Station
+	VALUES("catalunya", "open", "cat", "placa catalunya", 4, "barcelona" );
+    
+INSERT INTO Station
+	VALUES("arc de triumph", "open", "cat", "arc de triumph", 5, "barcelona" );
+    
